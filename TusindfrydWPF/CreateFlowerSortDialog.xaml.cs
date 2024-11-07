1a9e0b6642f12426dc5374d76a87da920dc774d3
averyden <igo408gt@gmail.com>
2024-11-07 09:15:00 +0100

    Implement UI updates

---
+++ TusindfrydWPF/CreateFlowerSortDialog.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using Daisies;
+
+namespace TusindfrydWPF
+{
+    /// <summary>
+    /// Interaction logic for CreateFlowerSortDialog.xaml
+    /// </summary>
+    public partial class CreateFlowerSortDialog : Window
+    {
+        public CreateFlowerSortDialog()
+        {
+            InitializeComponent();
+        }
+
+        public void updateOKBtn()
+        {
+            if(tbHalfLife.Text == "" || tbLineName.Text == "" || tbLineProductTime.Text == "" || tbSize.Text == "")
+            {
+                btnConfirmFlower.IsEnabled = false;
+            } else
+            {
+                btnConfirmFlower.IsEnabled = true;
+            }
+        }
+
+        
+
+        private void btnConfirmFlower_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void onChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            updateOKBtn();
+        }
+    }
+}
