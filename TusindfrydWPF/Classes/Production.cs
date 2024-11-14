e18d2c5f281e277146c474bd11c4bca2cca31fed
averyden <igo408gt@gmail.com>
2024-11-14 09:23:15 +0100

    xir.. this proejct sucks ass! i give up

---
+++ TusindfrydWPF/Classes/Production.cs
@@ -0,0 +1,30 @@
+﻿using Daisies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TusindfrydWPF.Classes
+{
+    public class Production
+    {
+        public DateTime StartDate { get; set; }
+
+        public int StartAmt { get; set; }
+
+        public int ExpectedAmt { get; set; }
+        public bool IsCompleted { get; set; }
+
+        //implicit constructor here
+
+        public void StartProd(string greenHouseName, string prodTray, FlowerSort flower, int startAmt)
+        { // Start date, start amount and expected date.
+            StartDate = DateTime.Now;
+            StartAmt = startAmt;
+            // 
+
+
+        }
+    }
+}
---
+++ TusindfrydWPF/Repos/FlowerRepository.cs
@@ -0,0 +1,26 @@
+﻿using Daisies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TusindfrydWPF.Repos
+{
+    public class FlowerRepository
+    {
+        public List<FlowerSort> Flowers { get; set; }
+        public FlowerRepository() { Flowers = new List<FlowerSort>(); }
+
+        public void AddFlower(FlowerSort flower)
+        {
+            Flowers.Add(flower);
+        }
+
+        public void SendToProd(FlowerSort flower)
+        {
+            
+        }
+
+    }
+}
